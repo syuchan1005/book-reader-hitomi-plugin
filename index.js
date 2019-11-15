c783a103261b2bf2a674b7364bbdcf2262dfa808
syuchan1005 <syuchan.dev@gmail.com>
2019-11-15 13:37:37 +0900

    :tada: Init

---
+++ index.js
@@ -0,0 +1,24 @@
+const gql = require('graphql-tag');
+
+const plugin = {
+  models: undefined,
+  typeDefs: gql`
+    type Mutation {
+        addHitomila(number: String! id: String!) Result!
+    }
+  `,
+  init(models) {
+    plugin.models = models;
+  },
+  middleware: {
+    Query: () => ({
+      addHitomila: (parent, { number, id }) => {
+        return {
+          success: true,
+        };
+      },
+    }),
+  },
+};
+
+module.exports = plugin;
