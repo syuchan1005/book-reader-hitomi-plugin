43c773783699f4e37343a98e529bf93b2c737119
syuchan1005 <syuchan.dev@gmail.com>
2019-11-15 22:42:52 +0900

    :construction: add

---
+++ hitomi.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const hitomi = {
+  urlFromUrlFromHash: (gId, image) => hitomi.urlFromUrl(hitomi.urlFromHash(gId, image)),
+  urlFromHash: (gId, image) => (!image.hash)
+    ? `//a.hitomi.la/galleries/${gId}/${image.name}`
+    : `//a.hitomi.la/images/${hitomi.fullPathFromHash(image.hash)}${path.extname(image.name)}`,
+  fullPathFromHash: (hash) => (hash.length < 3) ? hash : hash.replace(/^.*(..)(.)$/, `$2/$1/${hash}`),
+  urlFromUrl: (url, base) => url.replace(/\/\/..?\.hitomi\.la\//, `//${hitomi.subDomainFromUrl(url, base)}.hitomi.la/`),
+  subDomainFromUrl: (url, base) => {
+    let subDomain = base || 'a';
+    let m = /\/galleries\/\d*(\d)\//.exec(url);
+    let b = 10;
+    if (!m) {
+      b = 16;
+      m = /\/images\/[0-9a-f]\/([0-9a-f]{2})\//.exec(url);
+      if (!m) return subDomain;
+    }
+
+    const g = parseInt(m[1], b);
+    if (isNaN(g)) return subDomain;
+
+    return `${hitomi.subDomainFromGalleryId(g)}${subDomain}`;
+  },
+  subDomainFromGalleryId: (g) => String.fromCharCode(97 + (g % 3 /* common.js > number_of_frontends */)),
+};
+
+module.exports = hitomi;
--- index.js
+++ index.js
@@ -1,21 +1,127 @@
+const fs = require('fs').promises;
+
 const gql = require('graphql-tag');
+const axios = require('axios').default;
+const uuidv4 = require('uuid/v4');
+
+const hitomi = require('./hitomi');
+const util = require('./util');
 
 const plugin = {
-  models: undefined,
-  typeDefs: gql`
-    type Mutation {
-        addHitomila(number: String! id: String!): Result!
-    }
-  `,
-  init(models) {
-    plugin.models = models;
+  db: undefined,
+  typeDefs: gql`type Mutation {
+      addHitomi(id: ID! number: String! url: String!): Result!
+  }`,
+  init(db) {
+    plugin.db = db;
   },
   middleware: {
     Mutation: () => ({
-      addHitomila: (parent, { number, id }) => {
-        return {
-          success: true,
-        };
+      addHitomi: async (parent, { id, number, url }) => {
+        /* BookInfo check */
+        const bookInfo = await plugin.db.BookInfoModel.findOne({
+          where: { id },
+        });
+        if (!bookInfo) {
+          return {
+            success: false,
+            message: 'info not found',
+          };
+        }
+
+        /* extract id */
+        const numbers = url.match(/\d+/g);
+        if (numbers.length === 0) {
+          return {
+            success: false,
+            message: 'hitomi\'s id not found',
+          };
+        }
+        const galleryId = numbers[numbers.length - 1];
+
+        /* get gallery info */
+        let gallery = [];
+        try {
+          const galleryInfo = await axios.get(`https://ltn.hitomi.la/galleries/${galleryId}.js`)
+            .then(({ data }) => data);
+          eval(galleryInfo.replace('var galleryinfo', 'gallery'));
+        } catch (e) {
+          return {
+            success: false,
+            message: 'page hashes not found',
+          };
+        }
+        if (!Array.isArray(gallery)) {
+          return {
+            success: false,
+            message: 'gallery data is not valid',
+          };
+        }
+        const imageUrls = `https:${gallery.map((image) => hitomi.urlFromUrlFromHash(galleryId, image))}`;
+        const pad = imageUrls.length.toString(10).length;
+
+        /* write files */
+        const bookId = uuidv4();
+        const tempDir = `storage/book/${bookId}`;
+        await util.asyncForEach(imageUrls, async (url, i) => {
+          const filePath = `${tempDir}/${i.toString().padStart(pad, '0')}.jpg`;
+          const imageBuf = await axios.get(url, {
+            responseType: 'arraybuffer',
+          }).then(({ data }) => Buffer.from(data, 'binary'));
+          if (/\.jpe?g$/.test(url)) {
+            await fs.writeFile(filePath, imageBuf);
+          } else {
+            await (new Promise((resolve) => {
+              this.gm(imageBuf)
+                .quality(85)
+                .write(filePath, resolve);
+            }));
+          }
+        });
+
+        /* write database */
+        const bThumbnail = `/book/${bookId}/${'0'.padStart(pad, '0')}.jpg`;
+        await plugin.db.sequelize.transaction(async (transaction) => {
+          await plugin.db.BookModel.create({
+            id: bookId,
+            thumbnail: bThumbnail,
+            number,
+            pages: imageUrls.length,
+            infoId: id,
+          }, {
+            transaction,
+          });
+          await plugin.db.BookInfoModel.update({
+            // @ts-ignore
+            count: plugin.db.sequelize.literal('count + 1'),
+          }, {
+            where: {
+              id,
+            },
+            transaction,
+          });
+          await plugin.db.BookInfoModel.update({
+            history: false,
+            count: 1,
+          }, {
+            where: {
+              id,
+              history: true,
+            },
+            transaction,
+          });
+          await plugin.db.BookInfoModel.update({
+            thumbnail: bThumbnail,
+          }, {
+            where: {
+              id,
+              thumbnail: null,
+            },
+            transaction,
+          });
+        });
+
+        return { success: true };
       },
     }),
   },
